e396c98bc6c087047cd826d109a1ea1422154097
Andrii-888 <122465780+Andrii-888@users.noreply.github.com>
2023-03-17 16:18:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+// const d = 7; (const - Переменная значение которой нельзя переназначить)
+// console.log(d); (console.log - Это метод предназначенные для печати в консоль браузера)
+// console.log(d ** 2); (выводим значение в квадрате)
+// console.log(d ** 3); (выводим Значение в кубе )
+
+// let num1 = Number(prompt("Enter the number")); //(let-директива объявляет переменнyю,num1-переменая,prompt-тест для отображения и значение по умолчанию для поля ввода)
+
+// if (num1 > 0) {
+//   //(if-заполняет инструкцию если указанные условия выполняются истина )
+//   console.log("число положительное");
+//   console.log(num1 * 2);
+// } else if (num1 < 0) {
+//   //(else if-если не выполнено предыдущие условия но выполнено текущая )
+//   console.log(" число отрицательное");
+// } else {
+//   //(else- Чтобы указать блок который будет выполнен если тоже условия false ложно)
+//   console.log("число = 0");
+// }
+
+// let a = Number(prompt("Enter the number"));
+// let b = Number(prompt("Enter the number"));
+
+// if (a > b) {
+//   console.log(a + b);
+// } else if (a < b) {
+//   console.log(a * b);
+// } else if ((a == b)) {
+//   console.log("числа одинаковые");
+// }
+
+// let num = Number(prompt("Enter the number"));
+// if (num < 0) {
+//   console.log(num * 2);
+// } else {
+//   console.log("ошибка");
+// }
+
+// let num1 = Number(prompt("Enter the number"));
+// let num2 = Number(prompt("Enter the number"));
+
+// if (num2 > num1) {
+//   console.log("стульев хватает");
+// } else if (num1 > num2) {
+//   console.log("стульев не хватает");
+// }
+
+// let arr = []; (arr- Это класс для упрощения работы с массивом )
+
+// console.log("start");
+
+// for (let i = 1; i <= 9; i++) {  ( for - Выражение выполняющаяся в конце Интерации цикла. Обычно используется для обновления или увеличение переменной счётчика )
+//   console.log(i);              (i - Индекс массива)
+// }
+// console.log("finish");
+
+//for (let i = 10; i <= 99; i++) {
+//  if (i % 5 == 0) console.log(i);
+// i - Переменая
+// i++ - Это увеличение числа переменная на один при каждой интарации цикла
+//}
+
+// let arr = [3,15,40,80,-15,2,43,77];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i]>=15){
+//     console.log(arr[i])
+//   }
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2==0){
+//     console.log(arr[i])
+//   }
+// }
+
+// for (let i = 1; i < arr.length; i++) {
+//   if (i% 3==0){
+
+//     console.log(arr[i])
+//   }
+// }
